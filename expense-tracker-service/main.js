fd6e0d0fbb0a3285c8fa5c10bb7e615a727b4cf3
satsuralala <satsuraltumurbat@gmail.com>
2024-08-16 16:14:16 +0800

    8/16

--- expense-tracker-service/main.js
+++ expense-tracker-service/main.js
@@ -6,57 +6,40 @@ const app = express();
 const port = 4000;
 
 app.use(cors());
-app.use(express.json());7665
-bbbb                  
+app.use(express.json());
 const content = fs.readFileSync("categories.json", "utf-8");
 
-const categories = JSON.parse(content);
+let categories = JSON.parse(content);
 
-app.get("/categories/list", (req, res) => {
+app.get("/categories", (req, res) => {
   res.json(categories);
 });
 
-app.post("/categories/create", (req, res) => {
-  const { name } = req.body;
-  categories.push({ 
-    id:new Date().toISOString(),
-    name:name,
+app.post("/categories/#", (req, res) => {
+  const { name } = req.params;
+  categories.push({
+    id: new Date().toISOString(),
+    name: name,
   });
 
   fs.writeFileSync("categories.json", JSON.stringify(categories));
   res.json(["success"]);
 });
 
-
-
-
-app.put("/categories/update", (req, res) => {
-  const { id, name } = req.body;
-  const index=categories.findIndex((cat)=> cat.id===id);
+app.put("/categories/#", (req, res) => {
+  const { id, name } = req.params;
+  const index = categories.findIndex((cat) => cat.id === id);
   categories[index].name = name;
   fs.writeFileSync("categories.json", JSON.stringify(categories));
   res.json(["success"]);
 });
 
-
-app.delete("/categories/delete", (req, res) => {
-  const { id } = req.body;
-  categories=categories.filter(cat => cat.id !==id);
+app.delete("/categories/#", (req, res) => {
+  const { id } = req.params;
+  categories = categories.filter((cat) => cat.id !== id);
   fs.writeFileSync("categories.json", JSON.stringify(categories));
+  res.json(["success"]);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- expense-tracker-web/app/page.js
+++ expense-tracker-web/app/page.js
@@ -1,27 +1,22 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Badge } from "@/components/ui/badge"
-import { buttonVariants } from "@/components/ui/button"
-import { Button } from "@/components/ui/button"
-
-
-
-
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { buttonVariants } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const [categories, setCategories] = useState([]);
 
   function loadlist() {
-    fetch(`http://localhost:4000/categories/list`)
+    fetch(`http://localhost:4000/categories`)
       .then((res) => res.json())
       .then((data) => {
         console.log("data", data);
         setCategories(data);
       });
   }
-
 
   useEffect(() => {
     loadlist();
@@ -30,15 +25,13 @@ export default function Home() {
   function createNew() {
     const name = prompt("name");
 
-    fetch(`http://localhost:4000/categories/create`,{
-      method:"POST",
-      body: JSON.stringify({name: name}),
-      headers:{ 
-        "Content-type":"application/json; charset=UTF-8",
+    fetch(`http://localhost:4000/categories/#`, {
+      method: "POST",
+      body: JSON.stringify({ name: name }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
       },
-
-    } )
-
+    })
       .then((res) => res.json())
       .then(() => {
         loadlist();
@@ -46,36 +39,38 @@ export default function Home() {
   }
 
   function Delete(index) {
-    if(window.confirm("Do u want to delete this?")){
-      fetch(`http://npm i nodemonlocalhost:4000/categories/delete`,{
-        method:"DELETE",
-        body:JSON.stringify({index:index}),
+    if (confirm("Do u want to delete this?")) {
+      fetch(`http://localhost:4000/categories/#`, {
+        method: "DELETE",
+        body: JSON.stringify({ id: index }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
       })
-      .then((res) => res.json())
-      .then(() => {
-        loadlist();
-      });
-    }
-  }
-    function Edit(index,oldName) {
-      const name = prompt("name",oldName);
-      if(name){
-        fetch(`http://localhost:4000/categories/edit`,{
-          method:"PUT",
-          body:JSON.stringify({index:index})
-
-        })
         .then((res) => res.json())
         .then(() => {
           loadlist();
         });
+    }
+  }
 
-      }
-        
-  
-  
-      
-    fetch(`http://localhost:4000/categories/create?name=${name}`)
+  function Edit(id, oldName) {
+    const name = prompt("name", oldName);
+    if (name) {
+      fetch(`http://localhost:4000/categories/#`, {
+        method: "PUT",
+        body: JSON.stringify({ id: id,name: name}),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((res) => res.json())
+        .then(() => {
+          loadlist();
+        });
+    }
+
+    fetch(`http://localhost:4000/categories`)
       .then((res) => res.json())
       .then(() => {
         loadlist();
@@ -85,20 +80,23 @@ export default function Home() {
   return (
     <main>
       <button onClick={createNew}>add new</button>
-      {categories.map((category,index) => (
+      {categories.map((category, index) => (
         <div className="py-5 px-5 ">
-          <div key={category.name} className="text-black pb-3">{category.name}</div>
-          <Button variant="outline" className="mr-4" onClick={()=>Edit(index,category.name)}>edit</Button>
-          <Button variant="destructive" onClick={()=>Delete(index)} >delete</Button>
-
+          <div key={category.id} className="text-black pb-3">
+            {category.name}
+          </div>
+          <Button
+            variant="outline"
+            className="mr-4"
+            onClick={() => Edit(category.id, category.name)}
+          >
+            edit
+          </Button>
+          <Button variant="destructive" onClick={() => Delete(category.id)}>
+            delete
+          </Button>
         </div>
-       
       ))}
-   
-
-
-    
-
     </main>
   );
 }
