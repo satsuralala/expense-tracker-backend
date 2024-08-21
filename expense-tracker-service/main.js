6c31d2d025fb5c0d6ece4a79af71e4314b74e233
satsuralala <satsuraltumurbat@gmail.com>
2024-08-21 17:55:47 +0800

    8/21

--- expense-tracker-service/main.js
+++ expense-tracker-service/main.js
@@ -1,39 +1,17 @@
-const express = require("express");
-const cors = require("cors");
-const fs = require("fs");
-const { v4: uuidv4 } = require("uuid");
 
-const app = express();
-const port = 4000;
+const {startApp}=require("./configs/basic");
+const {sql}=require("./configs/database");
 
-app.use(cors());
-app.use(express.json());
+const { updateCategories ,deleteCategories, createNewCategory , getCategories} = require("./services/catService");
 
-let categories = JSON.parse(fs.readFileSync("categories.json", "utf-8"));
+const app=startApp();
 
-async function createNewCategory({ name }) {
-  let id = uuidv4();
-  categories.push({ id, name });
-  fs.writeFileSync("categories.json", JSON.stringify(categories));
-  return id;
-}
 
-function updateCategories(id, name) {
-  const index = categories.findIndex((cat) => cat.id === id);
-  categories[index].name = name;
-  fs.writeFileSync("categories.json", JSON.stringify(categories));
-}
-function deleteCategories(id) {
-  const deleteIndex = categories.findIndex((cat) => cat.id == id);
-  categories = categories.filter((cat) => cat.id !== id);
-  fs.writeFileSync("categories.json", JSON.stringify(categories));
 
-  return deleteIndex;
-}
 
-app.get("/categories", (req, res) => {
-  const categories = JSON.parse(fs.readFileSync("categories.json", "utf-8"));
-  res.json(categories);
+app.get("/categories", async (req, res) => {
+  const list =await getCategories();
+  res.json(list);
 });
 
 app.post("/categories", async (req, res) => {
@@ -42,28 +20,35 @@ app.post("/categories", async (req, res) => {
   res.status(201).json({ id, name });
 });
 
-app.put("/categories/:id", (req, res) => {
+app.put("/categories/:id", async(req, res) => {
   const { id } = req.params;
   const { name } = req.body;
   if (!name) {
     res.status(400).json({ error: "name required" });
-  } else {
-    updateCategories(id, name);
-    res.status(205).json({ message: "successful" });
-  }
+    return;
+  } 
+  await updateCategories(id, name);
+  res.sendStatus(204);
+
 });
 
-app.delete("/categories/:id", (req, res) => {
+app.delete("/categories/:id", async (req, res) => {
   const { id } = req.params;
 
   const deleteIndex = deleteCategories(id);
   if (deleteIndex < 0) {
     res.sendStatus(404);
-  } else {
-    res.sendStatus(204);
-  }
+    return;
+  } 
+  await deleteCategories(id);
+  res.sendStatus(204);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+
+app.get("/dbtest", async(req, res)=>{
+  const result=await sql`select version()`;
+  console.log(result);
+  res.json({result});
+})
+
+
---
+++ expense-tracker-service/tables.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE IF NOT EXISTS playing_with_neon(id SERIAL PRIMARY KEY, name TEXT NOT NULL, value REAL);
+
+CREATE TABLE categories(
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL
+)
+
+select id, name FROM categories;
+select * FROM categories;
+
+
+INSERT INTO playing_with_neon(name, value)
+  SELECT LEFT(md5(i::TEXT), 10), random() FROM generate_series(1, 10) s(i);
+SELECT * FROM playing_with_neon;
