85418d8b002e9922aa1bb3810007058e76fe896b
satsuralala <satsuraltumurbat@gmail.com>
2024-08-29 16:29:17 +0800

    8/29

--- expense-tracker-web/components/nav.js
+++ expense-tracker-web/components/nav.js
@@ -1,17 +1,28 @@
 import { Plus } from "lucide-react";
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+  } from "@/components/ui/avatar"
 
 export function Nav(){
     return(
-        <div className="max-w-[1200px] flex justify-center" >
-            <div className="flex gap-6 py-4">
-                <img src="/icon.svg" alt="Icon" width="28" height="28"/>
+        <div className="max-w-[1200px] flex items-center  px-[120px] " >
+            <div className="flex  gap-6 py-4  items-center">
+                <img src="/icon.svg" alt="Icon" width="40" height="40"/>
                 <div className="text-state-900 text-base font-semibold font-roboto">Dashboard</div>
                 <div className="font-normal text-base font-roboto">Records</div>
-            </div>
-            <div>
-                <button className="rounded-xl"><Plus />Record</button>
-            </div>
                 
+            </div>
+            <div className="flex gap-6 py-4  ml-auto" >
+                <button className="rounded-full bg-[#0166FF] flex   text-white py-2 px-3 pl-"><Plus />Record</button>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+          
+            </div>
+
         </div>
     )
 }
