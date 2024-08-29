d0255fccd155b837ba764efaa156d80c0ffc84ac
satsuralala <satsuraltumurbat@gmail.com>
2024-08-29 20:00:45 +0800

    8/29

--- expense-tracker-web/components/addRecord.js
+++ expense-tracker-web/components/addRecord.js
@@ -20,87 +20,89 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
+import { Separator } from "@/components/ui/separator"
 
 export function AddRecord() {
+  const [openRecord, setopenRecord] = useState(false);
+  const [closeRecord, setcloseRecord] = useState(false);
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <button className="font-normal text-base flex gap-2 text-[#1F2937] pt-3">
-          <Plus color="#0166FF" /> Add category
-        </button>
-      </DialogTrigger>
+    <>
+      <button className="font-normal text-base flex gap-2 text-[#1F2937] pt-3"
+        onClick={() => setopenRecord(true)}
+      >
+        <Plus color="#0166FF" /> Add category
+    </button>
 
-      <DialogContent className="w-[400px]">
-        <DialogHeader>
-          <DialogTitle>Add Category</DialogTitle>
-          <DialogDescription>
-            Select a tab and make changes as needed.
-          </DialogDescription>
-        </DialogHeader>
 
-        <Tabs defaultValue="account" className="w-full">
-          <TabsList className="grid w-full grid-cols-2">
-            <TabsTrigger value="account">Account</TabsTrigger>
-            <TabsTrigger value="password">Password</TabsTrigger>
-          </TabsList>
-          <TabsContent value="account">
-            <Card>
-              <CardHeader>
-                <CardTitle>Account</CardTitle>
-                <CardDescription>
-                  Make changes to your account here. Click save when you're done.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="space-y-2">
-                <div className="space-y-1">
-                  <Label htmlFor="name">Name</Label>
-                  <Input id="name" defaultValue="Pedro Duarte" />
-                </div>
-                <div className="space-y-1">
-                  <Label htmlFor="username">Username</Label>
-                  <Input id="username" defaultValue="@peduarte" />
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Button>Save changes</Button>
-              </CardFooter>
-            </Card>
-          </TabsContent>
-          <TabsContent value="password">
-            <Card>
-              <CardHeader>
-                <CardTitle>Password</CardTitle>
-                <CardDescription>
-                  Change your password here. After saving, you'll be logged out.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="space-y-2">
-                <div className="space-y-1">
-                  <Label htmlFor="current">Current password</Label>
-                  <Input id="current" type="password" />
-                </div>
-                <div className="space-y-1">
-                  <Label htmlFor="new">New password</Label>
-                  <Input id="new" type="password" />
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Button>Save password</Button>
-              </CardFooter>
-            </Card>
-          </TabsContent>
-        </Tabs>
+      <Dialog open={open}>
 
-        <DialogFooter>
-          <Button type="button" onClick={() => {}}>Close</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+        <DialogContent className="w-[400px]">
+          <DialogHeader>
+            <DialogTitle className="text-xl font-semibold text-[#0F172A]">Add Record</DialogTitle>
+            <Separator className="my-4" />
+          </DialogHeader>
+
+          <Tabs>
+            <TabsList  className="grid w-full grid-cols-2">
+              <TabsTrigger value="account" className="">Expense</TabsTrigger>
+              <TabsTrigger value="password">Income</TabsTrigger>
+            </TabsList>
+            <TabsContent value="account">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Account</CardTitle>
+                 
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <div className="space-y-1">
+                    <Label htmlFor="name">Name</Label>
+                    <Input id="name" defaultValue="Pedro Duarte" />
+                  </div>
+                  <div className="space-y-1">
+                    <Label htmlFor="username">Username</Label>
+                    <Input id="username" defaultValue="@peduarte" />
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <Button>Save changes</Button>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+            <TabsContent value="password">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Password</CardTitle>
+                  <CardDescription>
+                    Change your password here. After saving, you'll be logged
+                    out.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <div className="space-y-1">
+                    <Label htmlFor="current">Current password</Label>
+                    <Input id="current" type="password" />
+                  </div>
+                  <div className="space-y-1">
+                    <Label htmlFor="new">New password</Label>
+                    <Input id="new" type="password" />
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <Button>Save password</Button>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+          </Tabs>
+
+          <DialogFooter close={close}>
+            <Button type="button" onClick={() => setcloseRecord(true)}>
+              Close
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
